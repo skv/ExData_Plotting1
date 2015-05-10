f6ce9e04cbed3c70cf3c3db59387d7cabb8f5918
Sergey Skvortsov <skv@protey.ru>
2015-05-11 02:22:08 +0300

    remove 'cex' arg

--- plot4.R
+++ plot4.R
@@ -19,7 +19,7 @@ with(hpc2007, plot(datetime, Voltage, type="l", ylab="Voltage", xlab="datetime")
 with(hpc200702,	  plot(DateTime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="") )
 with(hpc200702, points(DateTime, Sub_metering_2, type="l", col="red") )
 with(hpc200702, points(DateTime, Sub_metering_3, type="l", col="blue") )
-legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), bty="n", cex=0.75)
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), bty="n")
 
 #screen(4)
 with(hpc2007, plot(datetime, Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime") )
